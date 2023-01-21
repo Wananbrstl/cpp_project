35c69595e201fb6ea902c2f5c2315e4124a9408b
YNan <745960184@qq.com>
2023-01-21 23:03:13 +0800

    add Point class

--- CG/Point.h
+++ CG/Point.h
@@ -1,9 +1,49 @@
 #ifndef POINT_H_
 #define POINT_H_ 
 
+#include <assert.h>
+#include <iostream>
+
+namespace lbn {
+
+/*   Point Class   */
 template<class T>
 class Point {
+private:
+    T x_, y_;
     
+public:
+    Point(T x, T y) :
+        x_(x), y_(y) {}
+
+    Point() : 
+        x_(T(0)), y_(T(0)) {}
+
+    ~Point() {}
+
+public:
+    T& X() {return x_;}
+    T& Y() {return y_;}
+
+    inline T X() const {return x_;}
+    inline T Y() const {return y_;}
+
+    T& operator[](int idx) {
+        assert(idx >= 0 && idx <= 1);
+        return idx == 0 ? x_ : y_;
+    }
+
+    template<class Pt_Ty>
+    friend std::ostream& operator<<(std::ostream& os, const Point<Pt_Ty>& pt);
+
 };
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const Point<T>& pt) {
+    os << "[" << pt.x_ << "," << pt.y_ << "]  ";
+    return os;
+}
+
+} // namespace 
 
 #endif // POINT _H_
---
+++ CG/utility.h
@@ -0,0 +1,21 @@
+#ifndef UTILITY_H_
+#define UTILITY_H_
+
+#include "common.h"
+
+namespace lbn {
+
+template<class T> 
+double area2(const Point<T>& p1, const Point<T>& p2, const Point<T>& p3) {
+    return (p2.X() - p1.X()) * (p3.Y() - p1.Y()) -
+           (p3.X() = p1.X()) * (p2.Y() - p1.Y());
+}
+
+template<class T>
+bool toLeftTest(const Point<T>& p1, const Point<T>& p2, const Point<T>& p3) {
+    return area2(p1, p2, p3) > 0;
+}
+
+} // namespace
+
+#endif // UTILITY_H_
