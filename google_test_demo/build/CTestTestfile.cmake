79fc707f980d62df0f6bcbd476dbe855467023c1
YNan <745960184@qq.com>
2023-01-25 17:48:54 +0800

    add cmake&googletest

---
+++ google_test_demo/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lbn/cpp_project/google_test_demo
+# Build directory: /home/lbn/cpp_project/google_test_demo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
---
+++ google_test_demo/build/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lbn/cpp_project/google_test_demo/test
+# Build directory: /home/lbn/cpp_project/google_test_demo/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
