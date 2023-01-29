fef3a46ea37f1181d4c9afbfe8a9c05c042c06e2
YNan <745960184@qq.com>
2023-01-29 14:40:24 +0800

    add .gitignore

--- google_test_demo/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lbn/cpp_project/google_test_demo/test
-# Build directory: /home/lbn/cpp_project/google_test_demo/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
